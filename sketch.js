73c4e8e9210dea10fcda5038030d5171b87be2b1
sota <55266382+sota14@users.noreply.github.com>
2020-03-01 21:43:57 +0900

    windowWidth&Hight

--- sketch.js
+++ sketch.js
@@ -1,16 +1,16 @@
 function setup() {
-      createCanvas(720,720);
+      createCanvas(windowWidth,windowHeight);
       console.log("Hello");
 
       noCursor();
-      colorMode(HSB,720,100,100);
+      colorMode(HSB,windowHeight,100,100);
       rectMode(CENTER);
       noStroke();
       }
 
       function draw() {
       background(mouseY/2,100,100);
-      fill(360-mouseY/2,100,100);
+      fill(windowWidth/2-mouseY/2,100,100);
       ellipse(mouseX,mouseY,100,100);
       frameRate(60)
       }
