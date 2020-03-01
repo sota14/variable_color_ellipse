ac1fe3b651a4edb670f1d15185af6ad82e5a1564
sota <55266382+sota14@users.noreply.github.com>
2020-03-01 21:55:05 +0900

    responsive

--- sketch.js
+++ sketch.js
@@ -1,16 +1,17 @@
 function setup() {
-      createCanvas(1920,1080);
+      createCanvas(windowWidth,windowHeight);
       console.log("Hello");
 
       noCursor();
-      colorMode(HSB,1080,100,100);
+      colorMode(HSB,windowHeight,100,100);
       rectMode(CENTER);
       noStroke();
       }
 
       function draw() {
       background(mouseY/2,100,100);
-      fill(540-mouseY/2,100,100);
+      fill(windowHight/2-mouseY/2,100,100);
       ellipse(mouseX,mouseY,100,100);
       frameRate(60)
       }
+
