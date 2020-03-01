0b195fc1a5289ba1947ec52f0a3141295e8fcba3
sota <55266382+sota14@users.noreply.github.com>
2020-03-01 22:17:55 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,14 +2,14 @@ function setup() {
       createCanvas(windowWidth,windowHeight);
 
       noCursor();
-      colorMode(HSB,windowHeight,100,100);
+      colorMode(HSB,900,100,100);
       rectMode(CENTER);
       noStroke();
       }
 
       function draw() {
       background(mouseY/2,100,100);
-      fill(windowHight/2-mouseY/2,100,100);
+      fill(450/2-mouseY/2,100,100);
       ellipse(mouseX,mouseY,100,100);
       frameRate(60)
       }
